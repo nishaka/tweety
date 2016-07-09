a23ae7f8860955fb01ff9b5c175bc0e2a3be4db3
nishaka <nisha.vittal03@gmail.com>
2016-07-09 22:34:44 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class EvenOdd
+{
+public static void main(Sring args[])
+{
+Scanner sc=new Scanner(System.in);
+int n;
+n=sc.nextInt();
+if(n%2==0)
+{
+System.out.println("Even");
+}
+else
+{
+System.out.println("Odd");
+}
+}
+}
