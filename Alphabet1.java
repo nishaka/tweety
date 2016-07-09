bf8df8e3601667c23b2e81fa33b3c4d1a312c6d9
nishaka <nisha.vittal03@gmail.com>
2016-07-09 22:58:07 +0530

    Add files via upload

---
+++ Alphabet1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Alphabet1
+{
+public static void main(String args[])
+{
+char n;
+n=sc.next.charat(0);
+if((n>='A'&&n<='Z')||(n>='a'&&n<='z'))
+{
+System.out.println("Alphabet");
+}
+else
+{
+System.out.println("Not alphabet");
+}}
+}
