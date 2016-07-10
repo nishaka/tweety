06019075e8e6827b73c062f9936d0aaa82284332
nishaka <nisha.vittal03@gmail.com>
2016-07-10 22:19:28 +0530

    Add files via upload

---
+++ Leap.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Leap
+{
+public static void main(String args[])
+{
+int year;
+Scanner sc=new Scanner(System.in);
+year=sc.nextInt();
+if(year%4==0)||(year%100==0)||(year%400==0)
+{
+System.out.println("Leap year");
+}
+else
+{
+System.out.println("Not Leap Year");
+}
+}
+}
