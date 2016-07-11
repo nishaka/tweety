92fd9798deb10f43e9fc43e217843f165e54a3bd
nishaka <nisha.vittal03@gmail.com>
2016-07-11 15:35:27 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Number
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n,sum=0;
+n=sc.nextInt();
+for(int i=0;i<n;i++)
+{
+sum=sum+i;
+}
+System.out.println(sum);
+}
+}
