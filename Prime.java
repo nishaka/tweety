1f7e0fe15c6f5d30774d8a611ba10a38078a72b0
nishaka <nisha.vittal03@gmail.com>
2016-07-12 19:28:39 +0530

    Add files via upload

--- Prime.java
+++ Prime.java
@@ -6,18 +6,18 @@ public static void main(String args[])
 int n,i,flag=0;
 Scanner sc=new Scanner(System.in);
 n=sc.nextInt();
-for(i=1;i<n;i++)
+for(i=2;i<n/2;i++)
 {
 if(n%i==0)
 {
+	System.out.println("Not Prime number");
 	flag=1;
+	
+	break;
 }
 }
-if(flag==1)
-{
-System.out.println("Not Prime number");
-}
-else
+
+if(flag==0)
 {
 System.out.println("Prime number");
 }
